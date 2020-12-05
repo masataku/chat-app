d6a0620aaeb4268403e520363d480674c7d681c7
masataka_omura <masataka.omura@icloud.com>
2020-12-05 19:20:04 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,9 +8,9 @@
 #  updated_at :datetime         not null
 #
 class Room < ApplicationRecord
-  has_many :user_rooms
+  has_many :user_rooms, dependent: :destroy
   has_many :users, through: :user_rooms
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 end
