597becebe73f410f6dc830dce1f20a33daf2c574
masataka_omura <masataka.omura@icloud.com>
2020-12-05 17:05:07 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,6 +10,7 @@
 class Room < ApplicationRecord
   has_many :user_rooms
   has_many :users, through: :user_rooms
+  has_many :messages
 
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,5 @@ class User < ApplicationRecord
 
   has_many :user_rooms
   has_many :rooms, through: :user_rooms
+  has_many :messages
 end
